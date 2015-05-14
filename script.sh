6f1d1052a736dd4747afac16cb0f7e3bd1c340f2
David Branner <brannerchinese@gmail.com>
2015-05-14 21:53:38 +0800

    add script

---
+++ script.sh
@@ -0,0 +1,6 @@
+cd <PATH TO REPO>/aipi_dihjyy
+# Get current IP address with curl ifconfig.me
+curl ifconfig.me > aipi_dihjyy
+git add -A <PATH TO REPO>/aipi_dihjyy/aipi_dihjyy
+git commit -m 'add aipi dihjyy'
+git push
